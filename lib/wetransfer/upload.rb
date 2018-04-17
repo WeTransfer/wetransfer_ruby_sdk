aa339f84a9623879bbb54635abd7cb519984cbb4
David Bosveld <david@wetransfer.com>
2018-04-17 15:22:27 +0200

    Remove error catching, will be done in next iteration and let the file processors open the file and complete them

--- lib/wetransfer/upload.rb
+++ lib/wetransfer/upload.rb
@@ -24,16 +24,14 @@ module WeTransfer
                         filesize: File.open(file).size
                         }
       end
+
       @transfer = WeTransfer::Transfers.new(@client).create_transfer(name: @options[:name],
                                                                         description: @options[:description],
                                                                         items: items_array)
-
       @transfer = WeTransfer::Transfers.new(@client).get_upload_urls(transfer: @transfer)
 
       handle_items
       puts @transfer.shortened_url
-    rescue => e
-      Hash[success: false, message: e.message]
     end
 
 
@@ -42,13 +40,11 @@ module WeTransfer
     def handle_items
       @files.each do |file|
         item_object = @transfer.items.select{|t| t['name'] == file.split('/').last}.first
-        file_object = File.open(file)
         if item_object['meta']['multipart_parts'] > 1
-          WeTransfer::Transfers.new(@client).multi_part_file(item: item_object ,file: file_object)
+          WeTransfer::Transfers.new(@client).multi_part_file(item: item_object ,file: file)
         else
-          WeTransfer::Transfers.new(@client).single_part_file(item: item_object ,file: file_object)
+          WeTransfer::Transfers.new(@client).single_part_file(item: item_object ,file: file)
         end
-        WeTransfer::Transfers.new(@client).complete_file(item: item_object)
       end
     end
   end

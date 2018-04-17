85947ce4ace47573b7ffd9f57412296b66b3126b
David Bosveld <david@wetransfer.com>
2018-04-17 15:22:47 +0200

    specs need to be there

--- spec/authorizer_spec.rb
+++ spec/authorizer_spec.rb
@@ -11,13 +11,23 @@ describe WeTransfer::Authorizer do
 
   context 'with a client' do
     before(:all) do
-      ENV['WT_API_URL'] = 'http://localhost:9001'
-      @client = WeTransfer::Client.new(api_key: 'sample-key')
+      ENV['WT_API_URL'] = 'https://55ckjvf49c.execute-api.eu-west-1.amazonaws.com/api/'
+      @client = WeTransfer::Client.new(api_key: 'e4PC5OmBU46O7oBBq8N3d8d6nudgABPN69wb1PUm')
     end
 
     it 'requests a jwt' do
       described_class.new(@client).request_jwt
       expect(@client.api_bearer_token).to_not be nil
     end
+
+    it 'returns a error when api-key is invalid' do
+      expect{
+        client = WeTransfer::Client.new(api_key: 'h4x0rz')
+        described_class.new(client).request_jwt
+
+      }.to raise_error(StandardError, /Authentication Failed/)
+    end
+
+
   end
 end
--- spec/transfers_spec.rb
+++ spec/transfers_spec.rb
@@ -25,7 +25,9 @@ describe WeTransfer::Transfers do
     end
 
     it 'creates a transfer without items' do
-      transfer = described_class.new(@client).create_new_transfer(name: 'Noah', description: 'has a test transfer')
+      transfer = described_class.new(@client).create_new_transfer(name: 'Noah',
+        description: 'has a test transfer')
+
       expect(transfer.id).to_not be nil
       expect(transfer.name).to eq('Noah')
       expect(transfer.description).to eq('has a test transfer')
@@ -34,12 +36,56 @@ describe WeTransfer::Transfers do
     end
 
     it 'creates a transfer with item listings' do
-      transfer = described_class.new(@client).create_new_transfer(name: 'Noah', description: 'has a test transfer', items: [{cool: 'great'}])
+      transfer = described_class.new(@client).create_new_transfer(name: 'Noah',
+        description: 'has a test transfer',
+        items: [{cool: 'great'}])
+
       expect(transfer.id).to_not be nil
       expect(transfer.name).to eq('Noah')
       expect(transfer.description).to eq('has a test transfer')
       expect(transfer.shortened_url).to_not be nil
       expect(transfer.items).to eq([{'cool' => 'great'}])
+    end
+
+    it 'creates a transfer with files and returns the file information' do
+      skip
+      transfer = described_class.new(@client).create_new_transfer(name: 'foo',
+        description: 'bar', items: [{local_identifier: 'qux', content_identifier: 'file', filename: 'quux', filesize: 1024}])
+
+      expect(transfer.id).to_not be(nil)
+      expect(transfer.name).to eq('foo')
+      expect(transfer.description).to eq('bar')
+      expect(transfer.shortened_url).to be_present?
+      expect(tramsfer.items.count).to eq(1)
+      expect(transfer.items['content_identifier']).to be('qux')
+      expect(transfer.items['meta']).to be_present?
+      expect(transfer.items['upload_url']).to be_present?
+      expect(transfer.items['size']).to be(1024)
+    end
+
+    it 'creates a transfer with a multipart file and returns the multipart information' do
+      skip
+      transfer = described_class.new(@client).create_new_transfer(name: 'foo',
+        description: 'bar', items: [{local_identifier: 'qux', content_identifier: 'file', filename: 'quux', filesize: 8_435_376}])
+
+      expect(transfer.id).to_not be(nil)
+      expect(transfer.name).to eq('foo')
+      expect(transfer.description).to eq('bar')
+      expect(transfer.shortened_url).to be_present?
+      expect(tramsfer.items.count).to eq(1)
+      expect(transfer.items['content_identifier']).to be('qux')
+      expect(transfer.items['meta']['multipart_parts']).to eq(2)
+      expect(transfer.items['size']).to be(8_435_376)
+    end
+
+    it 'should add more items to the transfer' do
+      skip
+      transfer = described_class.new(@client).create_new_transfer(name: 'foo',
+        description: 'bar', items: [{local_identifier: 'qux', content_identifier: 'file', filename: 'quux', filesize: 8_435_376}])
+      expect(transfer.items.size).to be(1)
+
+      transfer = described_class.new(@client).add_items(transfer: transfer, items: [{local_identifier: 'qux', content_identifier: 'file', filename: 'quux', filesize: 8_435_376}])
+      expect(transfer.items.size).to be(2)
     end
   end
 end

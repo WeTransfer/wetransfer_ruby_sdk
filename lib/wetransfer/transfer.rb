ae877f5dcec8906d02ee9f1d64b4a4d01c6fa225
David Bosveld <david@wetransfer.com>
2018-04-19 17:35:22 +0200

    added attributes to transfer class

--- lib/wetransfer/transfer.rb
+++ lib/wetransfer/transfer.rb
@@ -2,12 +2,42 @@ module WeTransfer
   class Transfer
     attr_accessor :id, :name, :description, :shortened_url, :items
 
-    def initialize(id:, name:, description: nil, shortened_url:, items: nil)
-      @id = id
-      @name = name
-      @description = description
-      @shortened_url = shortened_url
-      @items = items || []
+    def initialize
+      @items = []
     end
+
+    def build_items(items:)
+      items.each do |item|
+        item = ItemBuilder.build do |builder|
+          builder.set_path(path: item)
+          builder.set_content_identifier
+          builder.set_local_identifier
+          builder.set_name
+          builder.set_size
+        end
+        self.items.push(item)
+      end
+    end
+
+    def transfer_params
+      { name: name,
+        description: description,
+        items: items_params
+      }
+    end
+
+    def items_params
+      transfer_items = []
+      items.each do |item|
+        transfer_items << { local_identifier: item.local_identifier,
+                         content_identifier: item.content_identifier,
+                         filename: item.name,
+                         filesize: item.size
+                        }
+      end
+      transfer_items
+    end
+
+
   end
 end

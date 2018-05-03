9a5e962106e8dd7e2443587743bb67543e00610f
David Bosveld <david@wetransfer.com>
2018-05-03 15:42:46 +0200

    Renamed set_ methods to just what it sets

--- lib/wetransfer/transfer_builder.rb
+++ lib/wetransfer/transfer_builder.rb
@@ -8,16 +8,16 @@ module WeTransfer
                   end
     end
 
-    def set_details(name: nil, description: nil)
+    def name_description(name: nil, description: nil)
       @transfer.name = name || "File Transfer: #{Time.now.strftime('%d-%m-%Y')}"
       @transfer.description = description || 'Transfer generated with WeTransfer Ruby SDK'
     end
 
-    def self.set_id(transfer:, id:)
+    def self.id(transfer:, id:)
       transfer.id = id
     end
 
-    def self.set_shortened_url(transfer:, url:)
+    def self.shortened_url(transfer:, url:)
       transfer.shortened_url = url
     end
 

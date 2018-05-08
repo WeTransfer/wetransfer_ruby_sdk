07d42b38f84125a78c771102743217cf771c4a83
Julik Tarkhanov <me@julik.nl>
2018-05-08 14:26:00 +0200

    Continue

--- examples/we_transfer_client.rb
+++ examples/we_transfer_client.rb
@@ -1,16 +1,20 @@
 require 'faraday'
 require 'logger'
 require 'ks'
+require 'securerandom'
 
 class WeTransferClient
   NULL_LOGGER = Logger.new(nil)
 
   class FutureFileItem < Ks.strict(:name, :io)
     def initialize(*kwargs)
-      @uuid = SecureRandom.uuuid
+      super
+      @uuid = SecureRandom.uuid
     end
 
     def to_item_request_params
+      # Ideally the content identifier should stay the same throughout multiple
+      # calls if the file contents doesn't change.
       {
         content_identifier: 'file',
         local_identifier: @uuid,
@@ -36,14 +40,12 @@ class WeTransferClient
     end
   end
 
-  class RemoteTransfer
-    attr_accessor :items
-
+  class RemoteTransfer < Ks.strict(:name, :description, :items)
     def to_create_transfer_params
       {
         name: name,
         description: description,
-        items: Array(@items).map(&:to_item_request_params),
+        items: items.map(&:to_item_request_params),
       }
     end
 
@@ -53,15 +55,31 @@ class WeTransferClient
   end
 
   def initialize(api_key:, logger: NULL_LOGGER)
+    @api_url_base = 'https://dev.wetransfer.com'
     @api_key = api_key.to_str
+    @bearer_token = nil
     @logger = logger
   end
   
   def create_transfer(title:, message:)
     builder = TransferBuilder.new
     yield(builder)
-    xfer = RemoteTransfer.new
+    xfer = RemoteTransfer.new(name: title, description: message, items: Array(builder.items))
+    $stderr.puts xfer.inspect
+    create_and_upload(xfer)
     xfer
+  end
+
+  def create_and_upload(xfer)
+    authorize_if_no_bearer_token!
+    $stderr.puts xfer.to_create_transfer_params
+  end
+
+  def authorize_if_no_bearer_token!
+    return if @bearer_token
+    faraday = Faraday.new(@api_url_base)
+    response = faraday.post("/v1/authorize", {}, {'X-API-Key' => @api_key})
+    @bearer_token = JSON.parse(response.body).fetch('token')
   end
 
   def auth_headers

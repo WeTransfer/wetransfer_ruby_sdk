45291d1b50a935742327a1b72b556d1abf71f04e
David Bosveld <david@wetransfer.com>
2018-05-09 15:39:45 +0200

    changed path to file in example

--- examples/create_transfer.rb
+++ examples/create_transfer.rb
@@ -6,7 +6,7 @@ client = WeTransferClient.new(api_key: ENV.fetch('WT_API_KEY')) # , logger: Logg
 transfer = client.create_transfer(title: 'My amazing board', message: 'Hi there!') do |builder|
   builder.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
   builder.add_file(name: 'amazing.txt', io: StringIO.new('This is unbelievable'))
-  builder.add_file(name: 'huge.bin', io: File.open('/Users/julik/Downloads/0-Cassy.mp3', 'rb'))
+  builder.add_file(name: 'huge.bin', io: File.open('/path/to/local/file.jpg', 'rb'))
   builder.add_file_at(path: __FILE__)
 end
 

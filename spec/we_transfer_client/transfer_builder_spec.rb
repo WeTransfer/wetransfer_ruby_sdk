2241a69e25b8599cd95414a9c9af6f11d01ad205
Noah Berman <noah@noahberman.org>
2018-05-16 14:19:00 +0200

    Cop

--- spec/we_transfer_client/transfer_builder_spec.rb
+++ spec/we_transfer_client/transfer_builder_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe TransferBuilder do
   it 'fails if given an item with a size of 0' do
     broken = StringIO.new('')
-    expect { 
+    expect {
       described_class.new.ensure_io_compliant!(broken)
     }.to raise_error(/The IO object given to add_file has a size of 0/)
   end

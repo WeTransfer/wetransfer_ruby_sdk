a31097edaec3023550fad8f8e108d514fb90c4b4
Noah Berman <noah@noahberman.org>
2018-05-16 14:03:29 +0200

    Remind self how methods work

--- spec/we_transfer_client/transfer_builder_spec.rb
+++ spec/we_transfer_client/transfer_builder_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe TransferBuilder do
-  
+  it 'fails if given an item with a size of 0' do
+    broken = StringIO.new('')
+    expect { 
+      described_class.new.ensure_io_compliant!(broken)
+    }.to raise_error(/The IO object given to add_file has a size of 0/)
+  end
 end

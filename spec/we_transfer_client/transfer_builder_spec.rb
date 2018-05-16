ea542d73526cde6e694649eeb90356bae439fb8d
Noah Berman <noah@noahberman.org>
2018-05-16 15:54:13 +0200

    Cleanup, etc

--- spec/we_transfer_client/transfer_builder_spec.rb
+++ spec/we_transfer_client/transfer_builder_spec.rb
@@ -7,7 +7,7 @@ describe TransferBuilder do
       described_class.new.ensure_io_compliant!(broken)
     }.to raise_error(/The IO object given to add_file has a size of 0/)
   end
-  
+
   it 'raises if IO raises an error' do
     broken = []
 
@@ -15,14 +15,14 @@ describe TransferBuilder do
       described_class.new.ensure_io_compliant!(broken)
     }.to raise_error(TransferBuilder::TransferIOError)
   end
-  
+
   it 'adds a file' do
-    transfer_builder = described_class.new 
+    transfer_builder = described_class.new
     transfer_builder.add_file_at(path: __FILE__)
     expect(transfer_builder.items.count).to eq(1)
-    
+
     item = transfer_builder.items.first
-    expect(item.name).to eq("transfer_builder_spec.rb")
+    expect(item.name).to eq('transfer_builder_spec.rb')
     expect(item.io).to be_kind_of(File)
     expect(item.local_identifier).to be_kind_of(String)
   end

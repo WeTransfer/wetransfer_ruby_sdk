bac217d9c07224448f7a5d18ff3c7740e61456d3
David Bosveld <david@wetransfer.com>
2018-06-27 13:43:36 +0200

    Add feature to add file from url to

--- lib/we_transfer_client/transfer_builder.rb
+++ lib/we_transfer_client/transfer_builder.rb
@@ -18,7 +18,7 @@ class TransferBuilder
 
   def add_file_from_url(path:)
     image_path = save_to_file(path: path)
-    add_file_at(path: image_path)
+    add_file(name: File.basename(image_path), io: File.open(image_path, 'rb'))
   end
 
   def add_web_content(path:)
@@ -26,6 +26,18 @@ class TransferBuilder
     url_title = url.split('/').last
     @items << FutureWebItem.new(url: url, title: url_title)
     true
+  end
+
+  def save_to_file(path:)
+    # allow_redirection is needed to support http -> https redirection
+    url = open(path, allow_redirections: :safe).base_uri.to_s
+    file_name = url.split('/').last
+    extension = file_name.split('.').last
+    t = Tempfile.new([file_name, ".#{extension}"])
+    t.write(open(url).read)
+    t.flush
+    t.close
+    t.path
   end
 
   def ensure_io_compliant!(io)

028a7b4b97d91d7416944dc7a66c41ca13c460c4
David Bosveld <david@wetransfer.com>
2018-09-05 08:47:14 +0200

    Added descriptions per tests

--- spec/we_transfer_client/future_web_item_spec.rb
+++ spec/we_transfer_client/future_web_item_spec.rb
@@ -1,39 +1,44 @@
 require 'spec_helper'
 
 describe FutureWebItem do
-  it 'errors if not given all required arguments' do
-    expect {
-      described_class.new(url: 'https://www.wetransfer.com', local_identifier: '321235151')
-    }.to raise_error(/missing keyword: title/)
+  let (:params) { { url: 'http://www.wetransfer.com', title: 'WeTransfer' } }
 
-    expect {
-      described_class.new(title: 'wetransfer.com')
-    }.to raise_error(/missing keyword: url/)
+  describe '#initializer' do
+    it 'needs a :url keyword arg' do
+      params.delete(:url)
+      expect {
+        described_class.new(params)
+      }.to raise_error(ArgumentError, /url/)
+    end
+
+    it 'takes url when no title is given' do
+      params.delete(:title)
+      expect(described_class.new(params).title).to be(params.fetch(:url))
+    end
+
+    it 'succeeds if given all arguments' do
+      described_class.new(params)
+    end
   end
 
-  it 'succeeds if given all arguments' do
-    future_web_item = described_class.new(url: 'https://www.wetransfer.com', title: 'wetransfer.com', local_identifier: '321235151')
-    expect(future_web_item).to be_kind_of(FutureWebItem)
+  describe '#to_request_params' do
+    it 'creates params properly' do
+      as_params = described_class.new(params).to_request_params
+
+      expect(as_params[:url]).to eq('http://www.wetransfer.com')
+      expect(as_params[:meta][:title]).to be_kind_of(String)
+    end
   end
 
-  it 'succeeds if not passed a local_identifier' do
-    future_web_item = described_class.new(url: 'https://www.wetransfer.com', title: 'wetransfer.com')
-    expect(future_web_item).to be_kind_of(FutureWebItem)
-  end
+  describe 'getters' do
+    let(:subject) { described_class.new(params) }
 
-  it 'generates a local_identifier' do
-    future_web_item = described_class.new(url: 'https://www.wetransfer.com', title: 'wetransfer.com')
-    expect(future_web_item).to be_kind_of(FutureWebItem)
-    expect(future_web_item.local_identifier).to_not be nil
-    expect(future_web_item.local_identifier).to be_kind_of(String)
-  end
+    it '#url' do
+      subject.url
+    end
 
-  it 'creates params properly' do
-    future_web_item = described_class.new(url: 'https://www.wetransfer.com', title: 'wetransfer.com')
-    item_as_params = future_web_item.to_item_request_params
-    expect(item_as_params[:content_identifier]).to eq('web_content')
-    expect(item_as_params[:local_identifier]).to be_kind_of(String)
-    expect(item_as_params[:url]).to eq('https://www.wetransfer.com')
-    expect(item_as_params[:meta][:title]).to eq('wetransfer.com')
+    it '#title' do
+      subject.title
+    end
   end
 end

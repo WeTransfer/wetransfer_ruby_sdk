dad98fe7913f89d08776734aeaf6ef161a659734
David Bosveld <david@wetransfer.com>
2018-09-05 08:48:13 +0200

    disabled remote items

--- lib/we_transfer_client/remote_item.rb
+++ lib/we_transfer_client/remote_item.rb
@@ -1,2 +1,3 @@
-class RemoteItem < Ks.strict(:id, :local_identifier, :content_identifier, :name, :size, :mime_type, :url, :title)
-end
+# class RemoteItem < Ks.strict(:id, :local_identifier, :content_identifier, :name, :size, :mime_type, :url, :title)
+# end
+#

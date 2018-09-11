dda28fc2cbdfe01f88b6ae5f9b836f8067bfc625
David Bosveld <david@wetransfer.com>
2018-09-11 15:37:55 +0200

    add items to remote boards

--- spec/we_transfer_client/remote_board_spec.rb
+++ spec/we_transfer_client/remote_board_spec.rb
@@ -1,12 +1,31 @@
 require 'spec_helper'
 
 describe RemoteBoard do
-  let (:params) { { id: Random.rand(9999),
+  let (:params) { { id: [*('a'..'z'),*('0'..'9')].shuffle[0,36].join,
                     state: 'downloadable',
                     url: 'http://wt.tl/123abcd',
                     name:'RemoteBoard',
                     description: 'Test Description',
-                    items: []
+                    items: [{
+                              id: "s7l1urvgqs1b6u9v720180911093825",
+                              name: "board_integration_spec.rb",
+                              size: 3036,
+                              multipart: {
+                                part_numbers: 1,
+                                chunk_size: 3036
+                              },
+                              type: "file"
+                            },
+                            {
+                              id:"storr6ua2l1fsl8lt20180911093826",
+                              url:"http://www.wetransfer.com",
+                              meta:
+                                {
+                                  title: "WeTransfer Website"
+                                },
+                              type:"link"
+                            }
+                          ]
                   }}
 
   describe '#initializer' do
@@ -46,6 +65,35 @@ describe RemoteBoard do
       expect {
         described_class.new(params)
       }.to raise_error ArgumentError, /name/
+    end
+
+    it 'creates new object' do
+      described_class.new(params)
+    end
+
+    it 'creates classes for items' do
+      remote_board = described_class.new(params)
+      expect(remote_board.items.map{|i| i.class}).to eq([RemoteFile, RemoteLink])
+    end
+  end
+
+  describe 'getter' do
+    let (:subject) { described_class.new(params)}
+
+    it '#id' do
+      subject.id
+    end
+
+    it '#items' do
+      subject.items
+    end
+
+    it '#url' do
+      subject.url
+    end
+
+    it '#state' do
+      subject.state
     end
   end
 end

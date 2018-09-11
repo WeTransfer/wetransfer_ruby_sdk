b4bdfa37d751bb2fed4bdd82602543e6208082c3
David Bosveld <david@wetransfer.com>
2018-09-11 15:36:07 +0200

    Do one full test of all methods for boards

--- spec/board_integration_spec.rb
+++ spec/board_integration_spec.rb
@@ -3,138 +3,75 @@ require 'spec_helper'
 require_relative '../lib/we_transfer_client.rb'
 
 describe WeTransferClient do
-  let :test_logger do
+  TWO_CHUNKS_FILE_NAME = 'spec/testdir/two_chunks'
+
+  before(:all) do
+    Dir.mkdir('spec/testdir') unless Dir.exist?('spec/testdir')
+    unless File.exist?(TWO_CHUNKS_FILE_NAME)
+      File.open(TWO_CHUNKS_FILE_NAME, 'w') do |f|
+        f.puts('-' * (described_class::MAGIC_PART_SIZE + 3))
+        puts File.absolute_path(f)
+      end
+    end
+  end
+
+  let (:two_chunks) { File.open("#{Dir.pwd}/#{TWO_CHUNKS_FILE_NAME}", 'r') }
+
+  let (:test_logger) do
     Logger.new($stderr).tap { |log| log.level = Logger::WARN }
   end
 
-  let :very_large_file do
-    tf = Tempfile.new('test-upload')
-    20.times { tf << Random.new.bytes(1024 * 1024) }
-    tf << Random.new.bytes(rand(1..512))
-    tf.rewind
-    tf
-  end
-
-  let :client do
+  let (:client) do
     WeTransferClient.new(api_key: ENV.fetch('WT_API_KEY'), logger: test_logger)
   end
 
-  describe '.add_file_at' do
-    it 'should call #add_file' do
-      board = client.create_board(name: 'Test board', description: 'A board description') do |b|
-        expect(b).to receive(:add_file).with(name: anything, io: kind_of(::IO))
-
-        b.add_file_at(path: __FILE__)
-      end
+  it 'should create a board with files and web items as a block' do
+    # create a new board with a small file, bigger multipart file and a link
+    board = client.create_board(name: 'Test Board', description: 'Test description') do |b|
+      b.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
+      b.add_file(name: 'two_chunks', io: two_chunks)
+      b.add_web_url(url: 'http://www.wetransfer.com', title: 'WeTransfer Website')
     end
+    expect(board.items.map{|c|c.class}).to eq([RemoteFile, RemoteFile, RemoteLink])
+    expect(board.items[1].multipart.part_numbers).to be > 1
+    expect(board.items.count).to be(3)
 
-    it 'should accept a file path to be added to a board' do
-      board = client.create_board(name: 'test board', description: 'A board description') do |b|
-        b.add_file_at(path: __FILE__)
-      end
-      expect(board.items.count).to be(1)
+    # add more files to the board
+
+    client.add_items(board: board) do |b|
+      b.add_web_url(url: 'http://www.google.com', title: 'google')
+      b.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
     end
-  end
+    expect(board.items.count).to be(5)
+    expect(board.items.select{|i| i.type == 'file'}.count).to be(3)
+    expect(board.items.select{|i| i.type == 'link'}.count).to be(2)
 
-  it 'should create a empty board with name and description' do
-    board = client.create_board(name: 'Test board', description: 'A board description')
-    expect(board.name).to eq('Test board')
-    expect(board.description).to eq('A board description')
+    # actual uploading of the files to the board
+
+    # You have to upload every file one at the time, can't itterate over it
+    file_items = board.items.select{|i| i.type == 'file'}
+    # File items count is 3 so you have to execute this 3 times.
+    client.upload_file(board_id: board.id, file: file_items[0], io: File.open(__FILE__, 'rb'))
+    client.upload_file(board_id: board.id, file: file_items[1], io: two_chunks)
+    client.upload_file(board_id: board.id, file: file_items[2], io:  File.open(__FILE__, 'rb'))
+
     expect(board.url).to be_kind_of(String)
-    expect(board).to be_kind_of(RemoteBoard)
-    expect(board.items).to be_empty
-  end
+    response = Faraday.get(board.url)
+    # it hits the short-url with redirect
+    expect(response.status).to eq(302)
+    # but check in the header for a wetransfer domain location
+    expect(response['location']).to start_with('https://boards.wetransfer')
 
-  it 'should create a board with only a name' do
-    board = client.create_board(name: 'Test board', description: '')
-    expect(board.name).to eq('Test board')
-    expect(board.url).to be_kind_of(String)
-    expect(board).to be_kind_of(RemoteBoard)
-  end
-
-  it 'should return an error when board has no name' do
-    expect{
-      client.create_board(name: '', description: 'test description of board')
-    }.to raise_error(WeTransferClient::Error)
-  end
-
-  it 'should create a board with one file' do
-    board = client.create_board(name: 'Test board', description: 'A board description') do |b|
-      b.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
+    res = client.get_board(board_id: board.id)
+    if res.state == 'processing'
+      # it needs to be downloadable, otherwise something failed / or spaceship is slow
+      sleep(30)
+      res = client.get_board(board_id: board.id)
+      expect(res.state).to eq('downloadable')
     end
-    expect(board.items.first.class).to be(RemoteFile)
-    expect(board.items.first.name).to eq(File.basename(__FILE__))
-    expect(board.items.first.multipart.id).to_not be_empty
-    expect(board.items.count).to eq(1)
+    expect(res.state).to eq('downloadable')
+
+    expect(res.items.count).to be(5)
+    expect(res.items.first).to be_kind_of(RemoteFile)
   end
-
-  it 'should given an error when file not exists' do
-    board = client.create_board(name: 'Test board', description: 'A board description')
-    expect{
-      client.add_files(board: board) do |b|
-        b.add_file_at(path: '/path/to/non/existing/file.jpg')
-      end
-    }.to raise_error(Errno::ENOENT)
-  en
-
-  it 'should create a board with two items' do
-    board = client.create_board(name: 'Test board', description: 'A board description') do |b|
-      b.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
-      b.add_file(name: 'large.bin', io: very_large_file)
-    end
-    expect(board.items.map{|d|d.class}.uniq).to eq([RemoteFile])
-    expect(board.items.first.name).to eq(File.basename(__FILE__))
-    expect(board.items.first.multipart.id).to_not be_empty
-    expect(board.items.last.name).to eq('large.bin')
-    expect(board.items.count).to eq(2)
-  end
-
-  it 'should give an error when one of the files does not exists' do
-    ## Could be changed to accept the first two and raise an error on the third
-    board = client.create_board(name: 'Test board', description: 'A board description')
-    expect{
-      client.add_files(board: board) do |b|
-        b.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
-        b.add_file(name: 'large.bin', io: very_large_file)
-        b.add_file_at(path: '/path/to/non/existing/file.jpg')
-      end
-    }.to raise_error(Errno::ENOENT)
-  end
-
-  it 'should accept adding file to a existing board' do
-    board = client.create_board(name: 'Test board', description: 'A board description') do |b|
-      b.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
-    end
-    expect(board.items.count).to eq(1)
-
-    updated_board = client.add_items(board: board) do |b|
-      b.add_file(name: 'large_1.bin', io: very_large_file)
-      b.add_file(name: 'large_2.bin', io: very_large_file)
-    end
-    expect(updated_board.items.count).to be(3)
-    expect(updated_board.items.map{|d|d.class}.uniq).to eq([RemoteFile])
-    expect(updated_board.items.map{|d|d.id}.count).to be(3)
-  end
-
-  it 'should create a board with one web_item' do
-    skip
-  end
-
-  it 'should create a board with a file and web_item' do
-    skip
-  end
-
-  it 'should create a empty board and accept new file and web_items to be added' do
-    skip
-  end
-
-  it 'should return a board on request' do
-    skip
-  end
-
-  it 'should allow manual upload of files to boards' do
-    skip
-  end
-
-  # it 'should '
 end

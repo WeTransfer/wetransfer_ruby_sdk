0a96c36854c480e353ed8ad5ba7b8fea229de164
David Bosveld <david@wetransfer.com>
2018-09-26 12:33:52 +0200

    added feature tests

---
+++ spec/features/add_items_to_board_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+require_relative '../../lib/we_transfer_client.rb'
+
+describe WeTransfer::Client::Boards do
+  before(:all) do
+    Dir.mkdir('spec/testdir') unless Dir.exist?('spec/testdir')
+    unless File.exist?(TWO_CHUNKS_FILE_NAME)
+      File.open(TWO_CHUNKS_FILE_NAME, 'w') do |f|
+        f.puts('-' * (described_class::MAGIC_PART_SIZE + 3))
+        puts File.absolute_path(f)
+      end
+    end
+  end
+
+  let(:two_chunks) { File.open("#{Dir.pwd}/#{TWO_CHUNKS_FILE_NAME}", 'r') }
+
+  let(:test_logger) do
+    Logger.new($stderr).tap { |log| log.level = Logger::WARN }
+  end
+
+  let(:client) do
+    WeTransfer::Client.new(api_key: ENV.fetch('WT_API_KEY'), logger: test_logger)
+  end
+
+  let(:board) do
+    client.create_board(name: 'Test Board', description: 'Test Descritpion')
+  end
+
+  describe '#add_items' do
+    it 'adds items to a board' do
+      client.add_items(board: board) do |b|
+        b.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
+        b.add_file_at(path: "#{Dir.pwd}/#{TWO_CHUNKS_FILE_NAME}")
+        b.add_web_url(url: 'http://www.google.com', title: 'google')
+      end
+    end
+
+    it 'fails when no block is given' do
+      expect {
+        client.add_items(board: board)
+      }.to raise_error ArgumentError, /No items/
+    end
+
+    it 'fails when no board is passed as keyword argument' do
+      expect {
+        client.add_items do |b|
+          b.add_file_at(path: "#{Dir.pwd}/#{TWO_CHUNKS_FILE_NAME}")
+        end
+      }.to raise_error ArgumentError, /board/
+    end
+
+    it 'fails when file is not found' do
+      expect {
+        client.add_items(board: board) do |b|
+          b.add_file(name: 'file_not_found.rb', io: File.open('path/to/file.rb', 'r'))
+        end
+      }.to raise_error Errno::ENOENT, /No such file/
+    end
+
+    it 'fails when board is not a existing remote board' do
+      new_board = RemoteBoard.new(id: 123456, state: 'proccessing', url: 'https://www.we.tl/123456', name: 'fake board')
+      expect {
+        client.add_items(board: new_board) do |b|
+          b.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
+          b.add_file_at(path: "#{Dir.pwd}/#{TWO_CHUNKS_FILE_NAME}")
+          b.add_web_url(url: 'http://www.google.com', title: 'google')
+        end
+      }.to raise_error WeTransfer::Client::Error, /404 code/
+    end
+  end
+end
---
+++ spec/features/create_board_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+require_relative '../../lib/we_transfer_client.rb'
+
+describe WeTransfer::Client::Boards do
+  before(:all) do
+    Dir.mkdir('spec/testdir') unless Dir.exist?('spec/testdir')
+    unless File.exist?(TWO_CHUNKS_FILE_NAME)
+      File.open(TWO_CHUNKS_FILE_NAME, 'w') do |f|
+        f.puts('-' * (described_class::MAGIC_PART_SIZE + 3))
+        puts File.absolute_path(f)
+      end
+    end
+  end
+
+  let(:two_chunks) { File.open("#{Dir.pwd}/#{TWO_CHUNKS_FILE_NAME}", 'r') }
+
+  let(:test_logger) do
+    Logger.new($stderr).tap { |log| log.level = Logger::WARN }
+  end
+
+  let(:client) do
+    WeTransfer::Client.new(api_key: ENV.fetch('WT_API_KEY'), logger: test_logger)
+  end
+  describe '#create_board' do
+    it 'creates a remote board' do
+      client.create_board(name: 'Test Board', description: 'Test Descritpion')
+    end
+
+    it 'creates a board with items' do
+      client.create_board(name: 'Test Board', description: 'Test descrition') do |b|
+        b.add_file(name: File.basename(__FILE__), io: File.open(__FILE__, 'rb'))
+        b.add_file(name: 'two_chunks', io: two_chunks)
+        b.add_web_url(url: 'http://www.wetransfer.com', title: 'WeTransfer Website')
+      end
+    end
+
+    it 'fails when name is missing' do
+      expect {
+        client.create_board(name: '', description: 'Test Descritpion')
+      }.to raise_error WeTransfer::Client::Error, /400 code/
+    end
+
+    it 'fails when file path is wrong' do
+      expect {
+        client.create_board(name: 'Test Board', description: 'Test descrition') do |b|
+          b.add_file(name: 'file_not_found.rb', io: File.open('path/to/file.rb', 'r'))
+        end
+      }.to raise_error Errno::ENOENT, /No such file/
+    end
+
+    it 'fails when file name is missing' do
+      expect {
+        client.create_board(name: 'Test Board', description: 'Test descrition') do |b|
+          b.add_file(name: '', io: File.open(__FILE__, 'rb'))
+        end
+      }.to raise_error WeTransfer::Client::Error, /400 code/
+    end
+  end
+end

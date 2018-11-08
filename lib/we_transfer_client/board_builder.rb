80744e99823fb742173932aba8a60a71389048f4
David Bosveld <david@wetransfer.com>
2018-11-08 15:46:04 +0100

    Group items in file/link array and send file size instead of whole io

--- lib/we_transfer_client/board_builder.rb
+++ lib/we_transfer_client/board_builder.rb
@@ -1,23 +1,30 @@
 module WeTransfer
   class BoardBuilder
-    attr_reader :items
-    class TransferIOError < StandardError; end
+    attr_reader :items, :files, :links
 
     def initialize
-      @items = []
+      @files = []
+      @links = []
     end
 
-    def add_file(name:, io:)
-      @items << FutureFile.new(name: name, io: io)
+    def add_file(name:, size:)
+      @files << FutureFile.new(name: name, size: size)
     end
 
     def add_file_at(path:)
-      add_file(name: File.basename(path), io: File.open(path, 'rb'))
+      add_file(name: File.basename(path), size: File.size(path))
     end
 
     def add_web_url(url:, title: url)
-      @items << FutureLink.new(url: url, title: title)
+      @links << FutureLink.new(url: url, title: title)
     end
 
+    def items
+      (@files + @links).flatten
+    end
+
+    def select_file_on_name(name: )
+      file = files.select{|f| f.name == name}.first
+    end
   end
 end

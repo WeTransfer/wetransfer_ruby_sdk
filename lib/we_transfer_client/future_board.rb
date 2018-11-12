329660db93b2b1a756ee25ec340a2a79c9b810a7
David Bosveld <david@wetransfer.com>
2018-11-12 14:27:09 +0100

    remove unused methods

--- lib/we_transfer_client/future_board.rb
+++ lib/we_transfer_client/future_board.rb
@@ -1,34 +1,16 @@
 module WeTransfer
   class FutureBoard
-    attr_reader :name, :description, :items
+    attr_reader :name, :description
 
-    def initialize(name:, description: nil, items: [])
+    def initialize(name:, description: nil)
       @name = name
       @description = description
-      @items = items
-    end
-
-    def files
-      binding.pry
-      @items.select { |item| item.is_a?(WeTransfer::FutureFile) }
-    end
-
-    def links
-      @items.select { |item| item.is_a?(WeTransfer::FutureLink) }
     end
 
     def to_initial_request_params
       {
         name: name,
         description: description,
-      }
-    end
-
-    def to_request_params
-      {
-        name: name,
-        description: description,
-        items: items.map(&:to_request_params),
       }
     end
   end

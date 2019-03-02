bcf0a45d0e48720f024a538ad364015264c3c3c8
Arno <arno.fleming@gmail.com>
2019-03-02 16:07:43 +0100

    Enable more jrubies in CI

--- lib/we_transfer.rb
+++ lib/we_transfer.rb
@@ -5,7 +5,7 @@ require 'logger'
 require 'json'
 require 'ks'
 
-%w[communication_helper client transfer mini_io we_transfer_file remote_file version].each do |file|
+%w[communication client transfer mini_io we_transfer_file remote_file version].each do |file|
   require_relative "we_transfer/#{file}"
 end
 

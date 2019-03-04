73bfb10cc3f107c463c484c856dc740ebf62de5b
Arno <arno.fleming@gmail.com>
2019-03-04 21:56:22 +0100

    Use Forwardable over silly method defs that do just that

--- lib/we_transfer/mini_io.rb
+++ lib/we_transfer/mini_io.rb
@@ -2,8 +2,9 @@ module WeTransfer
   # Wrapper around an IO object, delegating only methods we need for creating
   # and sending a file in chunks.
   #
-
   class MiniIO
+    extend Forwardable
+
     def self.mini_io_able?(io)
       return false if io.is_a? WeTransfer::NullMiniIO
 
@@ -38,28 +39,6 @@ module WeTransfer
       @io.rewind
     end
 
-    def read(*args)
-      @io.read(*args)
-    end
-
-    def rewind
-      @io.rewind
-    end
-
-    def seek(*args)
-      @io.seek(*args)
-    end
-
-    # The size, delegated to io.
-    #
-    # nil is fine, since this method is used only as the default size for a
-    # WeTransferFile
-    # @returns [Integer] the size of the io
-    #
-    def size
-      @io.size
-    end
-
     # The name of the io, guessed using File.basename. If this raises a TypeError
     # we swallow the error, since this is used only as the default name for a
     # WeTransferFile
@@ -69,6 +48,8 @@ module WeTransfer
     rescue TypeError
       # yeah, what?
     end
+
+    def_delegators :@io, :read, :rewind, :seek, :size
 
     private
 
